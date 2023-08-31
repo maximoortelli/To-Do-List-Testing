634d713f4dfa8449885677c8a5ca29b6a2fc2564
Maximo Ortelli <118080546+maximoortelli@users.noreply.github.com>
2023-08-31 17:43:52 -0300

    Update TaskManager.test.js
    Update TaskManager.test.js

--- src/Jest/TaskManager.test.js
+++ src/Jest/TaskManager.test.js
@@ -2,8 +2,7 @@
  * @jest-environment jsdom
  */
 
-const TaskManager = require('./TaskManager-toTest.js');
-const injectLastTask = require('./addNewTasktoDOM.js');
+
 
 describe('TaskManager - add New Task to LocalStorage', () => {
   let taskList;
